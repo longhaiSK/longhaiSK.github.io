eb5652d58628348cc70757a14bba31953f46f4d2
Longhai Li <longhai.li@usask.ca>
2022-09-22 09:36:06 -0600

    delete s348

--- teaching/stat348_rdemo/Cluster/cluster_ex5-7.r
+++
@@ -1,8 +0,0 @@
-source ("estimates.r")
-coots <- read.csv ("data/coots.csv")
-
-cluster_ratio_est (data = coots, 
-				   cname = "clutch", csize = "csize", 
-				   yvar = "volume")
-
-
--- teaching/stat348_rdemo/UPS/upswr_ex6.6(studentshrs).R
+++
@@ -1,9 +0,0 @@
-source ("estimates.r")
-
-studenthrs <- read.table ("studentshrs.txt", header = T)
-
-## add sampling probability to the data
-studenthrs$pik <- studenthrs$class_size/647
-
-cluster_upswr_ratio_est (studenthrs, "sid" , "class_size", "pik", "hrs")
-
