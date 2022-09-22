6150cb93a35e16d17eabd65eb02db87ad02925e4
Longhai Li <longhai.li@usask.ca>
2022-09-22 09:36:06 -0600

    delete s348

--- teaching/stat348_rdemo/solutions/cluster_Q5.12(coots).R
+++
@@ -1,9 +0,0 @@
-source ("estimates.r")
-coots <- read.csv ("coots.csv")
-
-cluster_ratio_est (data = coots, 
-                   cname = "clutch", csize = "csize", 
-                   yvar = "length")
-
-
-
--- teaching/stat348_rdemo/solutions/cluster_Q5.3(jounal).R
+++
@@ -1,7 +0,0 @@
-source ("estimates.r")
-journal <- read.csv ("journal.csv")
-
-srs_ratio_est (journal$nonprob, journal$numemp)
-
-
-
