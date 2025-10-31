7b807575f6d97bbd976daf3261fc900e77a554dd
Longhai Li <longhai.li@usask.ca>
2025-10-31 10:11:25 -0600

    syn2GH commit from KNW5YYVTFD

--- teaching/stat845_rdemo/book/clean-chapters.lua
+++
@@ -1,9 +0,0 @@
--- Quarto Lua filter to demote all headers by one level.
--- This will change # to ##, ## to ###, and so on.
-
-function Header(el)
-  -- Increase the level of the header by 1
-  el.level = el.level + 1
-  return el
-end
-
