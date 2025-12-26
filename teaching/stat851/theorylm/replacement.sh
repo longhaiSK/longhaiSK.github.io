2d7eb6fdefc9c8d612406a0759a25fd70cae27b0
Longhai Li <51965323+longhaiSK@users.noreply.github.com>
2025-12-26 09:36:45 -0600

    syn2GH from Y3J7JJ9PPF

--- teaching/stat851/theorylm/replacement.sh
+++ teaching/stat851/theorylm/replacement.sh
@@ -3,10 +3,11 @@
 # '(?m)^(?!\s*(?:[-*]|\d+\.)\s)(.+)\n(?!\s*\n)(\s*(?:[-*]|\d+\.)\s)' \
 # '\1\n\n\2'
 
-regexrepl.py lec3-mvn.qmd \
-'(?m)^(?!\s*(?:[-*]|\d+\.)\s)(.+)\n(?!\s*\n)(\s*(?:[-*]|\d+\.)\s)' \
-'\1\n\n\2'
+# regexrepl.py lec3-mvn.qmd \
+# '(?m)^(?!\s*(?:[-*]|\d+\.)\s)(.+)\n(?!\s*\n)(\s*(?:[-*]|\d+\.)\s)' \
+# '\1\n\n\2'
 
+# sanitize enviroment name tags
 # regexrepl.py lec2-matrix.qmd \
 # '(:::\s*\{[^}]+)\}\s*\n\s*#{1,6}\s+(.+)' \
 # '\1 name="\2"}'
